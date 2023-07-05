178c79b11bed83af6344105fbd2a2bb8d948297a
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-05 17:05:17 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
-streamlit.header('Breakfast')
-streamlit.text('Spinach, Eggs')
-streamlit.text('Espresso, Bread')
-streamlit.text('Blueberry Oat meal & Mango smoothie')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Spinach,🐔 🥑 Eggs')
+streamlit.text('Espresso,🍞 Bread')
+streamlit.text('🥗Blueberry Oat meal & Mango smoothie')
+  
