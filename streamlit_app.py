8252fb1d2b51bae7bdacbb73e3c916b27f922c27
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-05 17:06:05 +0530

    Update streamlit_app.py
    Added one more header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Spinach,🐔 🥑 Eggs')
 streamlit.text('Espresso,🍞 Bread')
 streamlit.text('🥗Blueberry Oat meal & Mango smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
   
