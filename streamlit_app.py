658cdcfb8f2619c9dc638d3c5a0e0b4b48ad079a
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-05 17:08:16 +0530

    Update streamlit_app.py
    Imported pamdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Spinach,🐔 🥑 Eggs')
@@ -6,3 +7,5 @@ streamlit.text('Espresso,🍞 Bread')
 streamlit.text('🥗Blueberry Oat meal & Mango smoothie')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
   
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
