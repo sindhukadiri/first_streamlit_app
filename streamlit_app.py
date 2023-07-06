d3ee25ca4df2c4c752afc0294c36b1789806f20c
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-06 23:27:01 +0530

    Update streamlit_app.py
    Fruitvice addon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Spinach,🐔 🥑 Eggs')
@@ -19,3 +20,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
