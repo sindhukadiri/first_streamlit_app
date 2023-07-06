ddf1e446fcfd18efb705159f8efb67a069087931
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-06 23:32:03 +0530

    Update streamlit_app.py
    json removed

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # writes data to the screen
+#streamlit.text(fruityvice_response.json()) # writes data to the screen
 # takes the json and normalizes
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output on screen
