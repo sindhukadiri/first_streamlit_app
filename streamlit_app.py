f61ae503efea95169c7cc7c56dbf0accc0172d30
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-07 21:00:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('🥣 Spinach,🐔 🥑 Eggs')
 streamlit.text('Espresso,🍞 Bread')
 streamlit.text('🥗Blueberry Oat meal & Mango smoothie')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#import pandas  
+import pandas  
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json()) # writes data to the screen
 # takes the json and normalizes
