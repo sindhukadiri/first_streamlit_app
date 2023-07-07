e50ad0767f0963f6274b8be12b87b801d090d5a1
sindhukadiri <53306696+sindhukadiri@users.noreply.github.com>
2023-07-07 20:54:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 #import pandas
 #import requests
 import snowflake.connector
-import urllib.error import URLError
+from urllib.error import URLError
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Spinach,🐔 🥑 Eggs')
